5bb04d79b5cc584f36b0fee9959db4c1d30dcf8a
Nitish3118 <doomlok.here@gmail.com>
2025-09-04 10:13:08 +0530

    04/09/2025

--- src/exam/Examplestudentexam.java
+++ src/exam/Examplestudentexam.java
@@ -1,9 +1,10 @@
 package exam;
 import student.Student;
+
 public class Examplestudentexam extends Student{
 	public static void main(String args[]) {
 	Student s1= new Student (5, "Rohit", "AIML", 'C');
 	Examplestudentexam obj = new Examplestudentexam();
-	obj.Department="CS";
+//	obj.Department="CS";
 }
 }
